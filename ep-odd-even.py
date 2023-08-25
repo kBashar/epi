4629107acbb22105370810c2cb828d675d51aad0
kBashar <khyrulbashar.0.1@gmail.com>
2023-08-25 16:30:59 +0600

    Problem and solution with example for odd-even problem is added to the solution file

--- ep-odd-even.py
+++ ep-odd-even.py
@@ -1,3 +1,24 @@
+'''
+Problem: 
+    Check if a number is Odd or Even without using Modular arithmatic.
+
+Solution: 
+    Binary representation of a number holds 0 in its LSB(Least Significant Bit) if it
+    is an Even number. On the other hand LSB will be 1 if the number is Odd.
+
+    So, We can bitmask a number at LSB to check if the bit is set or not to tell if the
+    number is odd or even.
+
+Example:
+    Decimal     Binary      LSB     Type
+    ------------------------------------
+    5           101         1       Odd
+    6           110         0       Even
+    34          100010      0       Even
+    67          1000011     1       Odd
+
+'''
+
 number = int(input("Enter a number:"))
 
 # We bitmask to check if the LSB is `one` or `zero`.
